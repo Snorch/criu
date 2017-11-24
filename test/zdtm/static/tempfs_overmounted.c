10de19659bbd45c585c241de2a51b7034cca1e21
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2017-11-24 15:04:43 +0300

    zdtm: now tempfs_overmounted will pass so remove crfail
    changes in v2:
    make test check that file on overmounted mount is restored

--- test/zdtm/static/tempfs_overmounted.c
+++ test/zdtm/static/tempfs_overmounted.c
@@ -16,6 +16,9 @@ TEST_OPTION(dirname, string, "directory name", 1);
 
 int main(int argc, char **argv)
 {
+	char testfile[PATH_MAX];
+	int fd;
+
 	test_init(argc, argv);
 
 	mkdir(dirname, 0700);
@@ -23,6 +26,15 @@ int main(int argc, char **argv)
 		fail("Can't mount tmpfs");
 		return 1;
 	}
+
+	snprintf(testfile, PATH_MAX, "%s/testfile", dirname);
+	fd = open(testfile, O_CREAT | O_EXCL, 0600);
+	if (fd < 0) {
+		fail("Can't create file");
+		return 1;
+	}
+	close(fd);
+
 	if (mount("none", dirname, "tmpfs", 0, "") < 0) {
 		fail("Can't mount tmpfs");
 		return 1;
@@ -30,6 +42,16 @@ int main(int argc, char **argv)
 
 	test_daemon();
 	test_waitsig();
+
+	if (umount(dirname)) {
+		fail("Can't umount");
+		return 1;
+	}
+
+	if (access(testfile, F_OK) < 0) {
+		fail("Can't access testfile");
+		return 1;
+	}
 
 	pass();
 	return 0;
