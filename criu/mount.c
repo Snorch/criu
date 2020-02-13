6aa678104f385a274acaddfb255520b7b5b1604b
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2020-02-13 18:28:15 +0300

    ghost/mount: allocate remounted_rw in shmem to get info from other processes
    Previousely remounted_rw was not shared between all processes on
    restore, thus cleanup didn't got this info from rfi_remap and these
    mounts were wrongly left writable after restore.
    
    https://jira.sw.ru/browse/PSBM-101433
    
    v2: handle shmalloc error, fix spaces
    
    Fixes: fd0a3cd9efb9 ("mount: remount ro mounts writable before
    ghost-file restore")
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- criu/mount.c
+++ criu/mount.c
@@ -27,6 +27,7 @@
 #include "external.h"
 #include "clone-noasan.h"
 #include "fdstore.h"
+#include "rst-malloc.h"
 
 #include "images/mnt.pb-c.h"
 
@@ -1455,7 +1456,7 @@ err:
 	return -1;
 }
 
-static __maybe_unused int add_cr_time_mount(struct mount_info *root, char *fsname, const char *path, unsigned int s_dev)
+static __maybe_unused int add_cr_time_mount(struct mount_info *root, char *fsname, const char *path, unsigned int s_dev, bool rst)
 {
 	struct mount_info *mi, *t, *parent;
 	bool add_slash = false;
@@ -1474,7 +1475,7 @@ static __maybe_unused int add_cr_time_mount(struct mount_info *root, char *fsnam
 		}
 	}
 
-	mi = mnt_entry_alloc();
+	mi = mnt_entry_alloc(rst);
 	if (!mi)
 		return -1;
 
@@ -2790,7 +2791,7 @@ err_root:
 	return exit_code;
 }
 
-struct mount_info *mnt_entry_alloc()
+struct mount_info *mnt_entry_alloc(bool rst)
 {
 	struct mount_info *new;
 
@@ -2801,6 +2802,13 @@ struct mount_info *mnt_entry_alloc()
 
 	new = xzalloc(sizeof(struct mount_info));
 	if (new) {
+		if (rst) {
+			new->remounted_rw = shmalloc(sizeof(int));
+			if (!new->remounted_rw) {
+				xfree(new);
+				return NULL;
+			}
+		}
 		new->fd = -1;
 		new->is_overmounted = -1;
 		INIT_LIST_HEAD(&new->children);
@@ -2965,7 +2973,7 @@ static int collect_mnt_from_image(struct mount_info **head, struct mount_info **
 		if (ret <= 0)
 			break;
 
-		pm = mnt_entry_alloc();
+		pm = mnt_entry_alloc(true);
 		if (!pm)
 			goto err;
 
@@ -3248,7 +3256,7 @@ static int populate_mnt_ns(void)
 {
 	int ret;
 
-	root_yard_mp = mnt_entry_alloc();
+	root_yard_mp = mnt_entry_alloc(true);
 	if (!root_yard_mp)
 		return -1;
 
@@ -3261,7 +3269,7 @@ static int populate_mnt_ns(void)
 #ifdef CONFIG_BINFMT_MISC_VIRTUALIZED
 	if (!opts.has_binfmt_misc && !list_empty(&binfmt_misc_list)) {
 		/* Add to mount tree. Generic code will mount it later */
-		ret = add_cr_time_mount(root_yard_mp, "binfmt_misc", BINFMT_MISC_HOME, 0);
+		ret = add_cr_time_mount(root_yard_mp, "binfmt_misc", BINFMT_MISC_HOME, 0, true);
 		if (ret)
 			return -1;
 	}
@@ -3713,7 +3721,7 @@ int collect_mnt_namespaces(bool for_dump)
 				ret = -1;
 				goto err;
 			} else if (ret > 0 && add_cr_time_mount(ns->mnt.mntinfo_tree, "binfmt_misc",
-								BINFMT_MISC_HOME, s_dev) < 0) {
+								BINFMT_MISC_HOME, s_dev, false) < 0) {
 				ret = -1;
 				goto err;
 			}
@@ -3853,7 +3861,10 @@ int try_remount_writable(struct mount_info *mi, bool ns)
 	if (!ns)
 		remounted = REMOUNTED_RW_SERVICE;
 
-	if (mi->flags & MS_RDONLY && !(mi->remounted_rw & remounted)) {
+	/* All mounts in mntinfo list should have it on restore */
+	BUG_ON(mi->remounted_rw == NULL);
+
+	if (mi->flags & MS_RDONLY && !(*mi->remounted_rw & remounted)) {
 		if (mnt_is_overmounted(mi)) {
 			pr_err("The mount %d is overmounted so paths are invisible\n", mi->mnt_id);
 			return -1;
@@ -3876,7 +3887,7 @@ int try_remount_writable(struct mount_info *mi, bool ns)
 			if (call_helper_process(ns_remount_writable, mi))
 				return -1;
 		}
-		mi->remounted_rw |= remounted;
+		*mi->remounted_rw |= remounted;
 	}
 
 	return 0;
@@ -3891,7 +3902,7 @@ static int __remount_readonly_mounts(struct ns_id *ns)
 		if (ns && mi->nsid != ns)
 			continue;
 
-		if (!(mi->remounted_rw && REMOUNTED_RW))
+		if (!(*mi->remounted_rw && REMOUNTED_RW))
 			continue;
 
 		/*
