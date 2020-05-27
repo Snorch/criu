c087dd996e23c9baeca0a2d566ccc2a89fa4d6a7
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2020-05-27 11:50:34 +0300

    mount: add helper mnt_get_external_bind_nodev
    Will use it to find shared mount we can bind from and also can inherit
    external slavery. Device-external can't give us external slavery.
    
    Cherry-picked from Virtuozzo criu:
    https://src.openvz.org/projects/OVZ/repos/criu/commits/dcd952c4c
    
    Changes: switch to mnt_bind_pick helper.
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- criu/mount.c
+++ criu/mount.c
@@ -622,6 +622,19 @@ static bool mnt_is_root_bind(struct mount_info *mi)
 	return mnt_bind_pick(mi, __mnt_is_root_bind);
 }
 
+static bool __mnt_is_external_bind_nodev(struct mount_info *mi, struct mount_info *bind)
+{
+	if (bind->external && !mnt_is_dev_external(bind) && is_sub_path(mi->root, bind->root))
+		return true;
+
+	return false;
+}
+
+struct mount_info *mnt_get_external_bind_nodev(struct mount_info *mi)
+{
+	return mnt_bind_pick(mi, __mnt_is_external_bind_nodev);
+}
+
 /*
  * Having two children with same mountpoint is unsupported. That can happen in
  * case of mount propagation inside of shared mounts, in that case it is hard
