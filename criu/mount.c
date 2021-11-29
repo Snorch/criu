5eddaca273182f9741b778485ebb25461d198587
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2021-11-29 12:54:51 +0300

    mount: add mnt_bind_pick helper to pick the desired bind
    Adding different pick functions we would be able to search different
    things like mounted bind with wider root, or external bind, or external
    bind with same sharing group and so on and so forth.
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- criu/mount.c
+++ criu/mount.c
@@ -923,6 +923,28 @@ static void search_bindmounts(void)
 		__search_bindmounts(mi);
 }
 
+struct mount_info *mnt_bind_pick(struct mount_info *mi, bool (*pick)(struct mount_info *mi, struct mount_info *bind))
+{
+	struct mount_info *bind;
+
+	BUG_ON(!mi);
+
+	if (pick(mi, mi))
+		return mi;
+
+	/*
+	 * Shouldn't use mnt_bind list before it was setup in
+	 * __search_bindmounts
+	 */
+	BUG_ON(list_empty(&mi->mnt_bind) && !mi->mnt_no_bind);
+
+	list_for_each_entry(bind, &mi->mnt_bind, mnt_bind)
+		if (pick(mi, bind))
+			return bind;
+
+	return NULL;
+}
+
 static int resolve_shared_mounts(struct mount_info *info, int root_master_id)
 {
 	struct mount_info *m, *t;
