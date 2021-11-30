OpenVZ/vzcriu

mount: mount external mount before mounting it's binds
The problem when we don't order these mounts we can get to mounting
non-external bind first via do_new_mount and fail c/r. For instance for
tmpfs we fail on no image to get contents from. See the test
mnt_ext_root for more info.

https://jira.sw.ru/browse/PSBM-100080

Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

criu/mount.c
@@ -632,14 +632,14 @@ static bool mnt_needs_remap(struct mount_info *m)
 	return false;
 }
 
-static bool __mnt_is_external(struct mount_info *m, bool need_master)
-{
+static struct mount_info *__mnt_get_external(struct mount_info *m, bool need_master,
+					    bool need_mounted) {
 	struct mount_info *t;
 
 	BUG_ON(!m);
 
 	if (m->external)
-		return 1;
+		return m;
 
 	/*
 	 * Shouldn't use mnt_bind list before it was setup in
@@ -650,24 +650,30 @@ static bool __mnt_is_external(struct mount_info *m, bool need_master)
 	list_for_each_entry(t, &m->mnt_bind, mnt_bind)
 		if (t->external &&
 		    (!need_master || t->master_id == m->master_id) &&
+		    (!need_mounted || t->mounted) &&
 		    issubpath(m->root, t->root))
-			return 1;
+			return t;
 
-	return 0;
+	return NULL;
 }
 
 /*
  * Say mount is external if it was explicitly specified as an external or it
  * can be bind-mounted from such an explicit external mount.
  */
-static bool mnt_is_external(struct mount_info *m)
+static struct mount_info *mnt_is_external(struct mount_info *m)
 {
-	return __mnt_is_external(m, 0);
+	return __mnt_get_external(m, 0, 0);
 }
 
 static bool can_receive_master_from_external(struct mount_info *m)
 {
-	return __mnt_is_external(m, 1);
+	return __mnt_get_external(m, 1, 0);
+}
+
+static bool has_mounted_external_bind(struct mount_info *m)
+{
+	return __mnt_get_external(m, 0, 1);
 }
 
 /*
@@ -2783,6 +2789,8 @@ static bool rst_mnt_is_root(struct mount_info *m)
 
 static bool can_mount_now(struct mount_info *mi)
 {
+	struct mount_info *ext;
+
 	if (rst_mnt_is_root(mi)) {
 		pr_debug("%s: true as %d is mntns root\n", __func__, mi->mnt_id);
 		return true;
@@ -2793,6 +2801,12 @@ static bool can_mount_now(struct mount_info *mi)
 
 	if (mi->external)
 		goto shared;
+
+	if ((ext = mnt_is_external(mi)) && !has_mounted_external_bind(mi)) {
+		pr_debug("%s: false as %d's external %d is not mounted\n",
+			 __func__, mi->mnt_id, ext->mnt_id);
+		return false;
+	}
 
 	/*
 	 * We're the slave peer:

Snorch/criu

tmpfs we would fail on no image to get contents from. See the test
Cherry-picked from Virtuozzo criu:
https://src.openvz.org/projects/OVZ/repos/criu/commits/baf3f8db8
Changes: switch to mnt_bind_pick helper.

criu/mount.c
@@ -558,9 +558,14 @@ static bool __mnt_is_external_bind(struct mount_info *mi, struct mount_info *bin
  * Say mount is external if it was explicitly specified as an external or it
  * can be bind-mounted from such an explicit external mount.
  */
-static bool mnt_is_external_bind(struct mount_info *mi)
+static struct mount_info *mnt_get_external_bind(struct mount_info *mi)
 {
 	return mnt_bind_pick(mi, __mnt_is_external_bind);
+}
+
+static bool mnt_is_external_bind(struct mount_info *mi)
+{
+	return mnt_get_external_bind(mi);
 }
 
 static bool __can_receive_master_from_external(struct mount_info *mi, struct mount_info *bind)
@@ -574,6 +579,19 @@ static bool __can_receive_master_from_external(struct mount_info *mi, struct mou
 static bool can_receive_master_from_external(struct mount_info *mi)
 {
 	return mnt_bind_pick(mi, __can_receive_master_from_external);
+}
+
+static bool __has_mounted_external_bind(struct mount_info *mi, struct mount_info *bind)
+{
+	if (bind->external && bind->mounted && is_sub_path(mi->root, bind->root))
+		return true;
+
+	return false;
+}
+
+static bool has_mounted_external_bind(struct mount_info *mi)
+{
+	return mnt_bind_pick(mi, __has_mounted_external_bind);
 }
 
 /*
@@ -2372,6 +2390,8 @@ static bool rst_mnt_is_root(struct mount_info *m)
 
 static bool can_mount_now(struct mount_info *mi)
 {
+	struct mount_info *ext;
+
 	if (rst_mnt_is_root(mi)) {
 		pr_debug("%s: true as %d is mntns root\n", __func__, mi->mnt_id);
 		return true;
@@ -2382,6 +2402,11 @@ static bool can_mount_now(struct mount_info *mi)
 
 	if (mi->external)
 		goto shared;
+
+	if ((ext = mnt_get_external_bind(mi)) && !has_mounted_external_bind(mi)) {
+		pr_debug("%s: false as %d's external %d is not mounted\n", __func__, mi->mnt_id, ext->mnt_id);
+		return false;
+	}
 
 	/*
 	 * We're the slave peer: